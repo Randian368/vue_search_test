8c6f1ed24a95ac2a134e2eb78c955e9f6def5ec6
Eszter Pálmai <eszterp368@gmail.com>
2021-09-18 02:47:44 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<!-- <template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template> -->
+<template>
+  <div s="aaasd">
+    <div class="navbar navbar-expand-lg navbar-light bg-light">
+      <search-bar class="my-2 my-lg-0"></search-bar>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+      <search-bar></search-bar>
+      </div>
+      <div class="panel-body">
+        <p></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld.vue";
+import SearchBar from "./components/SearchBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    //HelloWorld,
+    SearchBar,
+  },
+};
+</script>
+
+<style scoped>
+.panel {
+   width: 480px;
+   height: 480px;
+   margin: 40px auto;
+ }
+
+ .panel > .panel-heading {
+   position:relative;
+ }
+
+.navbar .search-bar {
+  width:300px;
+  position: relative;
+  left: calc(100% - 300px);
+}
+</style>
---
+++ src/components/ProductSnippet.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="product-snippet">
+    <div class="image">
+      <img v-bind:src="'https://newoc3.drdkft.hu/image/' + product.image">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductSnippet",
+  props: {
+    product: Object,
+    title: String
+  }
+};
+</script>
+
+<style scoped>
+  div.product-snippet {
+    position: relative;
+    width: calc(100% - 32px);
+    background-color: #f4f4f4f4;
+    padding: 8px;
+    margin-left: 16px;
+  }
+  img {
+    width: 160px;
+    display:inline-block;
+  }
+
+
+</style>
